bacb29bdb9171aa822084c37b10362c55fcd9f84
JethroNatividad <natividad.jet@gmail.com>
2025-12-26 07:50:33 +0800

    feat: implement get minio object, write test for parse resume text from minio

--- apps/new-ai-worker/src/storage/minio_client.py
+++ apps/new-ai-worker/src/storage/minio_client.py
@@ -1,12 +1,13 @@
 from dotenv import load_dotenv
+import os
+from minio import Minio
 
 load_dotenv()
 
 _minio_client = None
+bucket_name = os.getenv("MINIO_BUCKET_NAME")
 
 def get_minio_client():
-    from minio import Minio
-    import os
     
     global _minio_client
     if _minio_client is None:
@@ -17,3 +18,18 @@ def get_minio_client():
             secure=False,  # Use True for HTTPS, False for HTTP
         )
     return _minio_client
+
+def get_minio_object(object_name):
+    """
+    Retrieve an object from MinIO storage.
+    """
+    client = get_minio_client()
+    try:
+        response = client.get_object(bucket_name, object_name)
+        data = response.read()
+        response.close()
+        response.release_conn()
+        return data
+    except Exception as e:
+        print(f"Error retrieving object {object_name} from bucket {bucket_name}: {e}")
+        return None
--- apps/new-ai-worker/tests/unit/resume_extraction/resume_extraction.py
+++ apps/new-ai-worker/tests/unit/resume_extraction/resume_extraction.py
@@ -1,6 +1,7 @@
 import os
 from src.services.resume_extraction import extract_pdf_text
 import time
+from src.storage.minio_client import get_minio_object
 
 # Test for pdf file
 def test_extract_pdf_text_from_file():
@@ -15,6 +16,24 @@ def test_extract_pdf_text_from_file():
     print("Extracted Text from File:")
     print(extracted_text)
 
-test_extract_pdf_text_from_file()
+# test_extract_pdf_text_from_file()
 
 # Test for pdf coming from minio
+
+def test_extract_pdf_text_from_minio():
+    object_name = "resumes/2025/taatat-1766095591385.pdf"
+    pdf_data = get_minio_object(object_name)
+    if pdf_data is None:
+        print(f"Failed to retrieve object {object_name} from MinIO.")
+        return
+
+    # calculate time taken to extract text
+    start_time = time.time()
+    extracted_text = extract_pdf_text(pdf_data)
+    end_time = time.time()
+    time_taken = end_time - start_time
+    print(f"Time taken to extract text from MinIO object: {time_taken} seconds")
+    print("Extracted Text from MinIO Object:")
+    print(extracted_text)
+
+test_extract_pdf_text_from_minio()
